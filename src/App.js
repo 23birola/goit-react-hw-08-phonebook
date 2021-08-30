b33bc555cc4aa7c4f4346a79f0a2eff55ba2a4e7
23birola <23birola@gmail.com>
2021-08-30 20:03:35 +0300

    added private route and public route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import { Switch, Route } from 'react-router-dom';
-// import toast, { Toaster } from 'react-hot-toast';
+import { Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 import AuthBar from './components/AuthBar/AuthBar';
 import css from './App.module.css';
-import { useEffect } from 'react';
+import { lazy, Suspense, useEffect } from 'react';
 import { fetchContacts } from './redux/contacts/contacts-operations';
 import authOperations from './redux/auth/auth-operations';
-import HomeView from './views/HomeView/HomeView';
-import SignUpView from './views/SignUpView/SignUpView';
-import LoginView from './views/LoginView/LoginView';
-import ContactsView from './views/ContactsView/ContactsView';
+
+import PublicRoute from './components/PublicRoute';
+import PrivateRoute from './components/PrivateRoute';
+const HomeView = lazy(() =>
+  import('./views/HomeView/HomeView' /* webpackChunkName: "HomePage" */),
+);
+const SignUpView = lazy(() =>
+  import('./views/SignUpView/SignUpView' /* webpackChunkName: "SignUpPage" */),
+);
+const LoginView = lazy(() =>
+  import('./views/LoginView/LoginView' /* webpackChunkName: "LoginPage" */),
+);
+const ContactsView = lazy(() =>
+  import(
+    './views/ContactsView/ContactsView' /* webpackChunkName: "ContactsPage" */
+  ),
+);
 
 export default function App() {
   const dispatch = useDispatch();
@@ -20,7 +32,7 @@ export default function App() {
   }, [dispatch]);
 
   useEffect(() => {
-  dispatch(fetchContacts());
+    dispatch(fetchContacts());
   }, [dispatch]);
 
   return (
@@ -28,10 +40,20 @@ export default function App() {
       <AuthBar />
 
       <Switch>
-        <Route exact path="/" component={HomeView} />
-        <Route path="/register" component={SignUpView} />
-        <Route path="/login" component={LoginView} />
-        <Route path="/contacts" component={ContactsView} />
+        <Suspense fallback={<p>Загружаем...</p>}>
+          <PublicRoute exact path="/">
+            <HomeView />
+          </PublicRoute>
+          <PublicRoute path="/register" restricted redirectTo="/contacts">
+            <SignUpView />{' '}
+          </PublicRoute>
+          <PublicRoute path="/login" restricted redirectTo="/contacts">
+            <LoginView />{' '}
+          </PublicRoute>
+          <PrivateRoute path={'/contacts'} redirectTo="/login">
+            <ContactsView />
+          </PrivateRoute>
+        </Suspense>
       </Switch>
     </div>
   );
